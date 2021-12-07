8ee1a2487a1eac44d915fb73646dc4bc0b744a54
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-12-07 15:26:30 -0600

    adjusted model to handle url errors

--- pathrise-back/app/models/opportunity.rb
+++ pathrise-back/app/models/opportunity.rb
@@ -1,21 +1,33 @@
 require 'pry'
 require 'json'
+require 'domainatrix'
 
 class Opportunity < ApplicationRecord
     self.primary_key = "id"
     
     def add_source
+        # binding.pry
+        if self.job_url.include?(' ')
+            self.job_url="http://www.google.com"
+        end
+        if self.job_url=="http://"
+            self.job_url="http://www.google.com"
+        end
         response = File.read("/Users/jjmarshall/pathrise/pathrise-back/public/jobBoards.json")
         @@data = JSON.parse(response)
-        names = @@data['job_boards'].map {|t| t['name'] }
-        my_url = self.job_url
-        my_url.slice!('https://www.')
-        stripped =  my_url.slice(0..(my_url.index('.com/'))).delete(".").capitalize
-
-        if names.include?(stripped)
-            source = names.select{|s| s == stripped}
-            self.source = source[0]
-        elsif self.job_url.include?(self.company_name.downcase) 
+        # names = @@data['job_boards'].map {|t| t['name'].downcase }
+        sites = @@data['job_boards'].map {|t| t['root_domain'] }
+        parsed_sites = sites.map{|s| Domainatrix.parse(s)}
+        domains = parsed_sites.map{|s| s.domain}
+        # board_sites = Domainatrix.parse(sites)
+        my_url = Domainatrix.parse(self.job_url)
+        domain = my_url.domain
+        # new_url = my_url.slice!('https://www.')
+        # stripped =  my_url.slice(0..(my_url.index('.com/'))).delete(".")
+        if domains.include?(domain)
+            source = domains.select{|d| d == domain}
+            self.source = source[0].capitalize
+        elsif domain.include?(self.company_name.downcase.gsub(/\s+/, "")) 
             self.source = "Company Website"
         else
             self.source = "Unknown"    
