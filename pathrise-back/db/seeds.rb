6a9a77b2a2a30d2f941fdef719b18a3c7a2579af
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-12-28 07:12:40 -0600

    asdfsad

--- pathrise-back/db/seeds.rb
+++ pathrise-back/db/seeds.rb
@@ -9,12 +9,12 @@ require 'json'
 Source.create({name: "Company Website", rating: "",root_domain: "",logo_file: "",description: "Unknown"})
 Source.create({name: "Unknown", rating: "",root_domain: "",logo_file: "",description: "Unknown"})
 
-response = File.read("/Users/jjmarshall/pathrise/pathrise-back/public/jobBoards.json")
+response = File.read("./public/jobBoards.json")
 data = JSON.parse(response)
 sources = data['job_boards'].map {|s| Source.create({name: s['name'], rating: s['rating'],root_domain: s['root_domain'],logo_file: s['logo_file'],description: s['description']}) }
 
 
 
-CSV.foreach('/Users/jjmarshall/pathrise/pathrise-back/lib/job_opportunities.csv', skip_blanks: true, nil_value: " ", empty_value: ' ', headers: true, :converters => [:all], :header_converters => [:downcase]) do |row|
+CSV.foreach('./lib/job_opportunities.csv', skip_blanks: true, nil_value: " ", empty_value: ' ', headers: true, :converters => [:all], :header_converters => [:downcase]) do |row|
     Opportunity.create!(row.to_hash)
 end
